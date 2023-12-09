82570e57368946e207df9f78eaec6ae7479f2185
chakradhar63 <vanarasi.chakradhar@iiitb.ac.in>
2023-12-09 23:00:15 +0530

    updated files

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,20 @@
-const express = require('express')
+const express = require('express');
 const cors = require("cors");
+const bodyParser = require('body-parser')
 const corsOptions = {
     origin:'http://localhost:3000',
     credentials: true,
     optionSuccessStatus: 200
-  }
-const app = express()
+}
+
+const app = express();
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+app.use(express.json());
+require("dotenv").config();
 const port = process.env.PORT || 5000
 const dbConnection = require('./db')
-app.use(express.json())
 
-app.use('/api/cars/' , require('./routes/carsRoute'))
-app.use('/api/users/' , require('./routes/usersRoute'))
-app.use('/api/bookings/' , require('./routes/bookingsRoute'))
 app.use(cors({
     origin: '*',
     methods: '*',   
@@ -25,6 +27,10 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Headers', '*');
     next();
 });
+
+app.use('/api/cars/' , require('./routes/carsRoute'))
+app.use('/api/users/' , require('./routes/usersRoute'))
+app.use('/api/bookings/' , require('./routes/bookingsRoute'))
 
 const path = require('path')
 
