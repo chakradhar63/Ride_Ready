0cac3ae2a55fc6aa0d7ef02a0508fd4bca9dd2ed
Visshall9 <vishal.cheeti@iiitb.ac.in>
2023-12-13 21:56:43 +0530

    add

--- backend/server.js
+++ backend/server.js
@@ -15,12 +15,14 @@ app.use(express.json());
 require("dotenv").config();
 const port = process.env.PORT || 5000
 const dbConnection = require('./db')
+const path = require('path')
 
 app.use(cors({
     origin: '*',
     methods: '*',   
     allowedHeaders: ['Content-Type', 'Authorization']
   }));
+
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin: *');
@@ -33,19 +35,13 @@ app.use('/api/cars/' , require('./routes/carsRoute'))
 app.use('/api/users/' , require('./routes/usersRoute'))
 app.use('/api/bookings/' , require('./routes/bookingsRoute'))
 
-const path = require('path')
-
 if(process.env.NODE_ENV==='production')
 {
 
     app.use('/' , express.static('client/build'))
-
     app.get('*' , (req , res)=>{
-
           res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
-
     })
-
 }
 
 app.get('/', (req, res) => res.send('Hello World!'))
